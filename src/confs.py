2ad97380af8ccea11d29c98a42ee29f9fee16411
Kristoffer Bäckman <kristoffer.backman@gmail.com>
2021-03-15 22:11:50 +0100

    dirty recording possible

--- src/confs.py
+++ src/confs.py
@@ -13,5 +13,30 @@ SOUND_UID = "NZ2"
 
 # -- database connections
 
+# -- variable names
 
+def create_var_names():
+    varnames = [f"spec{item}" for item in range(1,513)]
+    questions = ["?" for letter in range(1,515)]
+    return (varnames, questions)
+
+all_spectrums, all_questions = create_var_names()
+len(all_spectrums)
+len(all_questions)
+
+all_spectrums = ','.join(all_spectrums)
+all_spectrums
+all_questions = ','.join(all_questions)
+all_questions
+
+insert_query = f"""
+INSERT INTO decibel
+(tstamp,
+event,
+{all_spectrums})
+VALUES ({all_questions});
+"""
+
+with open("Output.sql", "w") as text_file:
+    text_file.write(insert_query)
 
