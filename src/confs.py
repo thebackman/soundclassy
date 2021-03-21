8a00e2a2388a3d45569ff1a5bfda009f05071484
Kristoffer Bäckman <kristoffer.backman@gmail.com>
2021-03-21 05:25:24 +0100

    add db connection to config

--- src/confs.py
+++ src/confs.py
@@ -1,19 +1,29 @@
 
+import os
+
 # -- paths
+
 
 proj_path = "/home/pi/Projects/soundclassy"
 
+
 # -- connections to tinkerforge
+
 
 HOST = "localhost"
 PORT = 4223
-
 MASTER_UID = "6Kvmoe"
 SOUND_UID = "NZ2"
 
+
 # -- database connections
 
+
+db_path = os.path.join(proj_path, "spectrum.db")
+
+
 # -- variable names
+
 
 def create_var_names():
     varnames = [f"spec{item}" for item in range(1,513)]
@@ -21,13 +31,13 @@ def create_var_names():
     return (varnames, questions)
 
 all_spectrums, all_questions = create_var_names()
-len(all_spectrums)
-len(all_questions)
+# len(all_spectrums)
+# len(all_questions)
 
 all_spectrums = ','.join(all_spectrums)
-all_spectrums
+# all_spectrums
 all_questions = ','.join(all_questions)
-all_questions
+# all_questions
 
 insert_query = f"""
 INSERT INTO decibel
@@ -37,6 +47,6 @@ event,
 VALUES ({all_questions});
 """
 
-with open("Output.sql", "w") as text_file:
-    text_file.write(insert_query)
+#with open("Output.sql", "w") as text_file:
+#    text_file.write(insert_query)
 
